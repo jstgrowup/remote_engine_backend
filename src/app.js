8a5e4e9d1e65e5647358b97a37d092ec6079bf7a
jstgrowup <deysubham999@gmail.com>
2023-12-17 20:59:36 +0530

    cors issues

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
