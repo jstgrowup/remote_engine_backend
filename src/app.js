54da6e2206b586857c95d9acce57c4ab27603576
jstgrowup <deysubham999@gmail.com>
2023-12-17 21:01:50 +0530

    fixes in cors

--- src/app.js
+++ src/app.js
@@ -5,18 +5,8 @@ import userRouter from "./routes/user.route.js";
 import developerOnboardingRouter from "./routes/developersOnboarding.route.js";
 import clientOnboardingRouter from "./routes/clientOnboarding.route.js";
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
